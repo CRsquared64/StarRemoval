3b447ca4a39d602032e67f09cd7bd77f9a46bb9f
Callum <Callumr714@icloud.com>
2021-06-28 16:44:51 +0100

    Jon why

--- MainAppGui.py
+++ MainAppGui.py
@@ -1,11 +1,15 @@
+import os
+os.environ["KCFG_KIVY_LOG_LEVEL"] = "trace"
 import cv2
 import kivy
+
+kivy.require('2.0.0')
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import ScreenManager
 from kivy.uix.widget import Widget
-
+from kivy_deps import sdl2, glew
 import ImageStar
 from MainScreen import MainScreen
 from kivy.config import Config
@@ -13,13 +17,11 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.image import Image
 import ImageStar
 
-Builder.load_file('Kv\MainScreen.kv')
-
+Builder.load_file('MainScreen.kv')
 
 
 class StarRemoval(App):
     def build(self):
-        Config.set('graphics', 'resizable', False)
         Window.size = 850, 800
         Window.minimum_width = 850
         Window.minimum_height = 800
@@ -35,16 +37,12 @@ class StarRemoval(App):
         self.root.get_screen("MainScreen").ids["giorno"].source = thresh
         self.thresh = thresh
 
-
     def process(self):
         imagename = self.imagename
         thresh = self.thresh
         thresh = float(thresh)
         ImageStar.RemoveStars(imagename, thresh)
         self.root.get_screen("MainScreen").ids["jojo"].source = 'finished.jpg'
-
-
-
 
 
 class sl(ScreenManager):
