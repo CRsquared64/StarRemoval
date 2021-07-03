801ea7e93bc4ef9962ac824bdd88ba5b0165aa17
GreenJon902 <GreenJon902@gmail.com>
2021-07-03 21:43:19 +0100

    removed uneaded functions

--- StarRemoval.py
+++ StarRemoval.py
@@ -51,33 +51,11 @@ class StarRemoval(App):
         Window.minimum_width = 850
         Window.minimum_height = 800
 
-        Clock.schedule_interval(lambda _elapsed_time: self.switch_image(), 1)
-        Clock.schedule_once(lambda _elapsed_time: self.on_image_paths(self, self.image_paths), 0)
-
         info_thread = Thread(target=systemInfo.computer_usage, args=(self.set_cpu_percent, self.set_memory_percent))
 
         info_thread.start()
 
         return SM()
-
-
-    def on_image_paths(self, _instance, current_image_paths):
-        layout: TabbedPanel = self.root.get_screen("MainScreen").ids["image_processing_info_layout"]
-
-        layout.clear_tabs()
-
-        if len(current_image_paths) == 0:
-            panel = TabbedPanelItem(text="Warning!")
-            panel.add_widget(Label(text="No images selected"))
-            layout.add_widget(panel)
-
-            Clock.schedule_once(lambda _elapsed_time: layout.switch_to(panel), 0)
-
-        else:
-            for path in current_image_paths:
-                layout.add_widget(ImageProcessingInfoItem(path=path))
-
-            Clock.schedule_once(lambda _elapsed_time: layout.switch_to(layout.tab_list[0]), 0)
 
 
     def add_image_path(self, path):
@@ -90,20 +68,6 @@ class StarRemoval(App):
 
         layout.add_widget(ImageProcessingInfoItem(path=path))
         Clock.schedule_once(lambda _elapsed_time: layout.switch_to(layout.tab_list[0]), 0)
-
-
-    def switch_image(self):
-        if len(self.image_paths) > 0:
-            current1 = self.image_paths.index(self.root.get_screen("MainScreen").ids["before_image"].source) \
-                if self.root.get_screen("MainScreen").ids["before_image"].source in self.image_paths else 0
-            self.root.get_screen("MainScreen").ids["before_image"].source = \
-                str(self.image_paths[(current1 + 1) % len(self.image_paths)])
-
-        if len(self.finished_image_paths) > 0:
-            current2 = self.finished_image_paths.index(self.root.get_screen("MainScreen").ids["after_image"].source) \
-                if self.root.get_screen("MainScreen").ids["after_image"].source in self.finished_image_paths else 0
-            self.root.get_screen("MainScreen").ids["after_image"].source = \
-                str(self.finished_image_paths[(current2 + 1) % len(self.finished_image_paths)])
 
 
     def open_file_explorer(self):
@@ -122,38 +86,6 @@ class StarRemoval(App):
     def set_threshold(self, thresh):
         Logger.info(f"Current threshold set to {thresh}")
         self.current_threshold = thresh
-
-
-    def process(self):
-        self.finished_image_paths.clear()
-
-        Logger.info("Starting to process image")
-
-        for path in self.image_paths:
-            self.amount_running += 1
-
-            Logger.info(f"Processing image from {path}")
-
-            thresh = float(self.current_threshold)
-
-
-            thread = Thread(target=starFunctions.remove_stars, args=(path, thresh, self.process_finished
-                                                                     ))
-            thread.start()
-            self.root.get_screen("MainScreen").ids["process_button"].disabled = True
-
-
-
-
-    @mainloop
-    def process_finished(self, path):
-        self.amount_running -= 1
-
-        Logger.info(f"Finished processing image {path}")
-        self.finished_image_paths.append(path)
-
-        if self.amount_running == 0:
-            self.root.get_screen("MainScreen").ids["process_button"].disabled = False
 
     @mainloop
     @next_frame  # Makes @mainloop redundant but it improves understandability because it does need to be both
